0cf02626d6b4597a997d614b634aea9d3441cac4
ryu shiroma <ryushiroma@gmail.com>
2020-06-26 19:46:52 -0300

    First commit

---
+++ build/precache-manifest.4763e09a7c4bd9507f7d89f7e602d522.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54d57cdf4a7d5c0d796b22d8b334b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f38e48200a6ac383a5d",
+    "url": "/static/js/2.130c8330.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.130c8330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3b8a4f968c9fcd3cac",
+    "url": "/static/js/main.f12181c5.chunk.js"
+  },
+  {
+    "revision": "adff22d36e27c879240b",
+    "url": "/static/js/runtime-main.d2851369.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,66 +1,28 @@
 import React, {useState} from 'react';
-//import './App.css'
-import styled , { css } from 'styled-components'
 
-const DivLoca = styled.div`
-    background-color: props.parameter;
-    height: 100px;
-    width: 100px;
-    border: 2px solid #000000;
-    margin:10px;
-`
+
 
 function App() {
-  const [quadrados, setQuadrados] = useState ([{color: 'blue'}])
- 
-  function createDiv(){
-    setQuadrados([...quadrados, {color:'blue'}])
-  } 
-
-  function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-   }
-   //var newColor = getRandomColor(); // #E943F0
-
-   function handleHover(index){
-    const novosQuadrados = [...quadrados]
-    novosQuadrados[index] = {color: getRandomColor()}
-    setQuadrados(novosQuadrados)
-    }
-
-  function handleLeave (index){
-    const novosQuadrados = [...quadrados]
-    novosQuadrados[index] = {color:'blue'}
-    setQuadrados(novosQuadrados)  
-  }
-
+  
+  
+  const maior18 = (idade) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (idade < 18) reject('Menor de 18 anos');
+    }, 2000);
+  });
+  
   return (
-    <div className="App">
-        <button onClick={createDiv}>Adicionar</button>    
-    
-        {
-          quadrados.map((elemento,index) =>   {
-          return (
-            <DivLoca
-            onMouseEnter={() => handleHover(index)} 
-            onMouseLeave={() => handleLeave(index)}
-            style={{backgroundColor: elemento.color}}
-            >
-                    Oi Mozi
 
-            </DivLoca>)
-          })
-        }
-    
-        
-        
-    </div>
-  );
+  <div>{
+    maior18(15)
+    .then(resp => console.log(`Sucesso: ${resp}`))
+    .catch(err => console.log(err))
+      }
+  </div>
+
+
+  )
+
 }
 
 export default App;
