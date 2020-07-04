4653f5280dbcde8978fe1fd8632b113c60b30f03
ryu shiroma <ryushiroma@gmail.com>
2020-07-04 12:58:38 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React, {useState} from 'react';
 
 
-
 function App() {
   
-  
+  const[idade,setIdade] = useState()
+
   const maior18 = (idade) => new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (idade < 18) reject('Menor de 18 anos');
-    }, 2000);
-  });
-  
-  return (
+      if (idade < 18) reject('Menor de 18 anos')
+      else resolve('Maior de 18 anos')
+    }, 2000)
 
-  <div>{
-    maior18(15)
+  })
+  
+  function confIdade (idade) {
+  
+     maior18(idade)
     .then(resp => console.log(`Sucesso: ${resp}`))
     .catch(err => console.log(err))
-      }
+ 
+  }
+
+  return (
+
+  <div>
+
+    <input type="number" onChange={(e) => setIdade(e.target.value)} value={idade}/>
+    <button onClick={()=> confIdade(idade)}>Conferir Idade</button>
+    
+    
+    
   </div>
 
 
